dbb8c03f5094e9e6e08167b1b0d75e4b8bd62ce3
mickmed <mick2090@gmail.com>
2020-01-14 23:49:32 -0500

    basic router structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './App.css'
 import {Footer} from '../Footer'
+import About from '../About'
+import Landing from '../Landing'
+import Story from '../Story'
+import Study from '../Study'
+import SwingStates from '../SwingStates'
 
 function App() {
   return (
@@ -13,6 +18,24 @@ function App() {
         The body
       </div>
       <Footer />
+      <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/landing">
+            <Landing />
+          </Route>
+          <Route path="/story">
+            <Story />
+          </Route>
+          <Route path="/study">
+            <Study />
+          </Route>
+          <Route path="/swingstates">
+            <SwingStates />
+          </Route>
+
+        </Switch>
     </div>
   );
 }
