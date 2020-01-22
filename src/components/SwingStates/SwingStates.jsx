e5dff0800ad8917ccd861b5fdddfde58f37ef410
mickmed <mick2090@gmail.com>
2020-01-22 11:58:55 -0500

    swingstates basic layout

--- src/components/SwingStates/SwingStates.jsx
+++ src/components/SwingStates/SwingStates.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
-
+import React from "react";
+import { NavLink } from 'react-router-dom'
+import AppHeader from "../AppHeader/AppHeader";
+import "./SwingStates.scss";
 
 const SwingStates = () => {
+  const swingStates = [
+    "Colorado",
+    "Florida",
+    "Georgia",
+    "Michigan",
+    "Minnesota",
+    "Nevada",
+    "New Hampshire",
+    "Ohio",
+    "Pennsylvania",
+    "Virginia"
+  ];
+  const swingList = swingStates.map(stateName => (
+    <div className="state-name"><NavLink className="state-name-link" activeClassName="active" to="/">{stateName}</NavLink></div>
+  ));
+  return (
+    <div className="swingstates-page">
+      <AppHeader />
+      
+      <main class="main-swing">
+      <div className="title-swing">Swing States</div>{swingList}
+      </main>
+    </div>
+  );
+};
 
-    return (
-        <div>This is the SwingStates Page</div>
-    )
-}
-
-export default SwingStates
+export default SwingStates;
