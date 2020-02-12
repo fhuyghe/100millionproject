85277b65777667152b822c3f565a54b42f003769
Sean Ryan <seanpryan89@gmail.com>
2020-02-11 21:34:32 -0500

    Slider submission working properly

--- src/components/Quiz/QuizSlider.jsx
+++ src/components/Quiz/QuizSlider.jsx
@@ -1,35 +1,42 @@
 import React, { Component } from "react";
-import "./quizSlider.scss"
-import "./quiz.scss"
-
+import "./quizSlider.scss";
 
 class QuizSlider extends React.Component {
   constructor() {
-    super()
-    this.state = {value: 3}
-    this.handleChange = this.handleChange.bind(this)
+    super();
+    this.state = { value: 3 };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
- 
+
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(){
+    console.log(this.props)
   }
 
   render() {
     return (
       <div>
+        <form>
           <div>Extremely Interested</div>
-        <label>
+          <div className="slider-wrapper">
           <input
             id="sliderInput"
+            className="slider"
             type="range"
-            min="0" max="10"
+            min="0"
+            max="10"
             value={this.state.value}
             onChange={this.handleChange}
             step="1"
-            orient="vertical"/>        
-        </label>
-        <div>Not Interested</div>
-        <button className="next-button" type="submit" for="sliderInput" >NEXT</button>
+          />
+          </div>
+          <div>Not Interested</div>
+          <input className="next-button" onClick={() => this.props.handleSliderSubmission(this.state.value)} type="submit" value="Next" />
+        </form>
       </div>
     );
   }
