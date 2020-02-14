610e08ab58f2ffa617170e05cf000d8b34dac154
Francois Huyghe <francois@13milliseconds.com>
2020-02-14 13:28:16 -0600

    Make react-router always scroll to top on page change

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import { App } from './components/App';
+import ScrollToTop from './components/Shared/ScrollToTop'
 // import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from "react-router-dom";
 
 
-ReactDOM.render(<Router><App/></Router>, document.getElementById('root'));
+ReactDOM.render(<Router><ScrollToTop /><App/></Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
